192c3262c66fb3a3b1f63bf612467aa91f59b0b9
Fredrik Ekre <fredrik.ekre@chalmers.se>
2016-11-18 16:40:58 +0100

    add files

--- src/Imports.jl
+++ src/Imports.jl
@@ -1,5 +1,67 @@
 module Imports
 
-# package code goes here
+export @Import, @Using
+
+"""
+```julia
+@Import MyLongModuleName as m
+```
+
+Load the module `MyLongModuleName` with `import` and binds it to `m`.
+Equivalent to
+
+```julia
+import MyLongModuleName
+const m = MyLongModuleName
+```
+"""
+:(@Import)
+
+"""
+```julia
+@Using MyLongModuleName as m
+```
+
+Load the module `MyLongModuleName` with `using` and binds it to `m`.
+Equivalent to
+
+```julia
+using MyLongModuleName
+const m = MyLongModuleName
+```
+"""
+:(@Using)
+
+# generate the macros
+for (macroname, operator) in ((:Import, "import"), (:Using, "using"))
+    @eval begin
+        macro $macroname(M::Union{Symbol, Expr}, kw::Symbol, m::Symbol)
+            # input check
+            kw == :as || throw(ArgumentError("syntax error: expected `as`, got `$kw`"))
+            isdefined(m) && throw(ArgumentError("alias `$m` already defined"))
+
+            # create operator expression
+            names = get_names(M)
+            ex = Expr(Symbol($operator))
+            for n in names
+                push!(ex.args, n)
+            end
+
+            return quote
+                $(esc(ex))
+                const $(esc(m)) = $(esc(M))
+                nothing
+            end
+        end
+    end
+end
+
+# utility function to extract names from expression
+function get_names(x)
+    isa(x, Symbol)   && return (x, )
+    x.head == :quote && return (x.args[1], )
+    x.head == :.     && return (get_names(x.args[1])..., get_names(x.args[2])...)
+    throw(ArgumentError("invalid module name"))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,70 @@
 using Imports
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+module A
+    export foo
+    foo() = true
+    Foo() = true
+    module B
+        export bar
+        bar() = true
+        Bar() = true
+        module C
+            export baz
+            baz() = true
+            Baz() = true
+        end
+    end
+end
+
+@testset "@Import" begin
+    # A
+    @test (@Import A as a) == nothing
+    @Import A as a
+    @test_throws UndefVarError foo()
+    @test_throws UndefVarError Foo()
+    @test a.foo()
+    @test a.Foo()
+
+    # A.B
+    @test (@Import A.B as ab) == nothing
+    @Import A.B as ab
+    @test_throws UndefVarError bar()
+    @test_throws UndefVarError Bar()
+    @test ab.bar()
+    @test ab.Bar()
+
+    # A.B.C
+    @test (@Import A.B.C as abc) == nothing
+    @Import A.B.C as abc
+    @test_throws UndefVarError baz()
+    @test_throws UndefVarError Baz()
+    @test abc.baz()
+    @test abc.Baz()
+end # testset
+
+@testset "@Using" begin
+    # A
+    @test (@Using A as a) == nothing
+    @Using A as a
+    @test foo()
+    @test_throws UndefVarError Foo()
+    @test a.foo()
+    @test a.Foo()
+
+    # A.B
+    @test (@Using A.B as ab) == nothing
+    @Using A.B as ab
+    @test bar()
+    @test_throws UndefVarError Bar()
+    @test ab.bar()
+    @test ab.Bar()
+
+    # A.B.C
+    @test (@Using A.B.C as abc) == nothing
+    @Using A.B.C as abc
+    @test baz()
+    @test_throws UndefVarError Baz()
+    @test abc.baz()
+    @test abc.Baz()
+end # testset
